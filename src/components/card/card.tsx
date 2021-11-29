27c2a4603ebeddcdef4b91fe9631f1d9a55a9943
Jay Pan <lb0104672@gmail.com>
2021-11-29 14:15:02 +0800

    feat(alias): introduce import alias

---
+++ src/components/card/card.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from 'react';
+import { View, Text } from 'react-native';
+
+export const Card: FC = () => {
+  return (
+    <View>
+      <Text>card hello world! test</Text>
+    </View>
+  );
+};
